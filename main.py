b33580a9cff2b0cd7c59c6e7b976b23eadb08897
waedkhalifa <62521979+waedkhalifa@users.noreply.github.com>
2021-12-23 20:25:51 +0200

    front ent part2, search by topic, cache

---
+++ main.py
@@ -0,0 +1,55 @@
+import json
+from itertools import cycle
+
+from flask import Flask, jsonify, request
+
+import requests
+
+app = Flask(__name__)
+
+
+catCycle = cycle(["http://192.168.56.104:7777","http://192.168.56.101:7000"])
+ordCycle = cycle(["http://192.168.56.105:9999","http://192.168.56.103:9000"])
+
+
+@app.route('/searchTopic/<string:topic>', methods=['GET'])
+def searchByBookTopic(topic):
+    f = open('cache.json', 'r')
+    data = json.load(f)
+
+    if topic in data["srchTopic"]:
+        for i in data["srchTopic"][topic]:
+             if data["srchTopic"][topic] != None:
+                   return jsonify(data["srchTopic"][topic])
+
+    req=requests.get("{}/search/{}".format(next(catCycle),topic)) #send to catalog
+    f.close()
+    if req.status_code == 200:
+        result=req.json() #content of json as dictionary
+
+        f3 = open('cache.json', 'r')
+        data = json.load(f3)
+        for x in range(len(result)):
+            if result[x]["id"] in data["srchTopic"]:
+                return jsonify(result)
+
+        data["srchTopic"][topic] = result
+        f3.close()
+
+        f2 = open('cache.json', 'w')
+        json.dump(data, f2)
+        f2.close()
+
+
+        return jsonify(result)
+
+    elif req.status_code == 404:
+        return 'The server has not found anything matching the given URL',404
+
+    else:
+        return 'Status code '+ str(req.status_code) +' indicates to something ERROR!',req.status_code
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=6000)
