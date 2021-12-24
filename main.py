88a8dc5ae054e58270402ce85099579c9c01dfa4
waedkhalifa <62521979+waedkhalifa@users.noreply.github.com>
2021-12-24 21:21:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -93,11 +93,11 @@ def purchase(id):
 
     if req.status_code == 200:
         result = req.json()  # content of json as dictionary
-        req2 = requests.get("{}/purchase/{}".format(next(ordCycle), id))
-        if req2.status_code == 200:
-            return '',req.status_code
-        else:
-            return jsonify(result)
+        #req2 = requests.get("{}/purchase/{}".format(next(ordCycle), id))
+        #if req2.status_code == 200:
+        #    return '',req.status_code
+        #else:
+        return jsonify(result)
 
     elif req.status_code == 404:
         return 'The server has not found anything matching the URI given',404
